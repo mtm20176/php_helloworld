7a4e55bf23445bc96cd3dc8af5a172bc3840ebb9
Mark Milligan <mtm20176@gmail.com>
2021-11-25 21:16:49 +0000

    other samples

---
+++ firstlineofcode.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link type='text/css' rel='stylesheet' href='style.css'/>
+    <title>Get Started!</title>
+</head>
+<body>
+<p><?php echo "My first line of PHP!"; ?></p>
+</body>
+</html>
